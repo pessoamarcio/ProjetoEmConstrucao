dfbc3132e671e4e83d380614009d412b9fb5576e
pessoamarcio <pessoa-marcio@hotmail.com>
2023-04-27 20:20:59 -0300

    Projeto em contrução para estudo de .Net

---
+++ ProjetoFromLuiz/Controllers/PessoaController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoFromLuiz.Repository;
+
+namespace ProjetoFromLuiz.Controllers
+{
+    public class PessoaController : Controller
+    {
+        private static PessoaRepository pessoaRepository;
+
+        public PessoaController()
+        {
+            pessoaRepository = new PessoaRepository();
+        }
+
+        [HttpPost]
+        [Route("api/pessoas")]
+        public IActionResult AdicionarPessoa([FromBody] Pessoa novaPessoa)
+        {
+            if (novaPessoa == null)
+            {
+                return BadRequest();
+            }
+
+            pessoaRepository.AdicionarPessoa(novaPessoa);
+
+            return Ok(novaPessoa);
+        }
+
+        [HttpGet]
+        [Route("api/pessoas")]
+        public IActionResult ListarTodasPessoas()
+        {
+            var pessoas = pessoaRepository.ListarTodos();
+            return Ok(pessoas);
+        }
+
+        [HttpGet]
+        [Route("api/pessoas/{id}")]
+        public IActionResult ListarPessoasId(int id)
+        {
+            var pessoa = pessoaRepository.ListarPessoaPorId(id);
+
+            if (pessoa == null)
+            {
+                return NotFound("Falhou!");
+
+            }
+
+            return Ok(pessoa);
+        }
+
+        [HttpPut]
+        [Route("api/pessoas/{id}")]
+        public IActionResult AtualizarPessoas(int id, [FromBody] Pessoa PessoaAtualizada)
+        {
+            if (PessoaAtualizada == null)
+            {
+                return BadRequest();
+
+            }
+
+            pessoaRepository.AtualizacaoDaPessoa(id, PessoaAtualizada);
+
+            return Ok("Cadastro atualizado com sucesso!");
+        }
+        
+        [HttpDelete]
+        [Route("api/pessoas/{id}")]
+        public IActionResult ExcluirPessoa(int id)
+        {
+            pessoaRepository.ExcluirPessoa(id);
+
+            return Ok("Cadastro Deletado!");
+        }
+    }
+}
---
+++ ProjetoFromLuiz/Repository/PessoaRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace ProjetoFromLuiz.Repository
+{
+    public class PessoaRepository
+    {
+        private static List<Pessoa> baseDePessoas = new List<Pessoa>
+        {
+            new Pessoa { Id = 1, Nome = "João", Idade = 25, Email = "joao@email.com"},
+            new Pessoa { Id = 2, Nome = "Maria", Idade = 30, Email = "maria@email.com"},
+            new Pessoa { Id = 3, Nome = "Pedro", Idade = 18, Email = "pedro@email.com"}
+        };
+
+        public void AdicionarPessoa(Pessoa novaPessoa)
+        {
+            novaPessoa.Id = baseDePessoas.Count + 1;
+            baseDePessoas.Add(novaPessoa);
+        }
+
+        public List<Pessoa> ListarTodos()
+        {
+            return baseDePessoas;
+        }
+
+        public Pessoa ListarPessoaPorId(int id)
+        {
+            return baseDePessoas.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void AtualizacaoDaPessoa(int id, Pessoa atualizacaoDaPessoa)
+        {
+            var pessoa = baseDePessoas.FirstOrDefault(p => p.Id == id);
+            if (pessoa != null)
+            {
+                pessoa.Nome = atualizacaoDaPessoa.Nome;
+                pessoa.Idade = atualizacaoDaPessoa.Idade;
+                pessoa.Email = atualizacaoDaPessoa.Email;
+            }
+
+        }
+
+        public bool AtualizacaoDaPessoaTeste(int id, Pessoa pessoaAtualizada)
+        {
+            var pessoa1 = baseDePessoas.FirstOrDefault(p => p.Id == id);
+
+            if (pessoa1 == null)
+            {
+                return false;
+            }
+
+            else
+            {
+                pessoa1.Nome = pessoaAtualizada.Nome;
+                pessoa1.Idade = pessoaAtualizada.Idade;
+                pessoa1.Email = pessoaAtualizada.Email;
+            }
+
+            return true;
+        }
+
+        public void ExcluirPessoa(int id)
+        {
+            var pessoa = baseDePessoas.FirstOrDefault(p => p.Id == id);
+
+            if (pessoa != null)
+            {
+                baseDePessoas.Remove(pessoa);
+            }
+        }
+    }
+}
